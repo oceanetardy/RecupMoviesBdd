65eed05fc4854c1b354adc105faa4e4e9610c3bf
Océane TARDY <tardy.oceane42@gmail.com>
2025-01-24 12:39:34 +0100

    🔧 Fake Config
    Ajout exemple de fichier de config

---
+++ configExemple.py
@@ -0,0 +1,6 @@
+API_KEY = "FAKE-API-KEY-1234-5678-ABCD-EFGH"
+DB_PASSWORD = "FAKE_DB_PASS_9876"
+MONGO_URI = f"mongodb+srv://fakeUser:fakePassword@cluster0.fake.mongodb.net/fakeDatabase?retryWrites=true&w=majority"
+DB_NAME = "FakeDbName"
+COLLECTION_NAME = "FakeCollection"
+
